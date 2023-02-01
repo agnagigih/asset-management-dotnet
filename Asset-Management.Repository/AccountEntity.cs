1294639aecbd3e392e8690999ddab2480ff0da58
Agna Gigih Priambodo <agna.gigih@protonmail.com>
2023-02-01 11:38:39 +0700

    first commit

---
+++ Asset-Management.Repository/AccountEntity.cs
@@ -0,0 +1,26 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace Asset_Management.Repository
+{
+    [Table("account")]
+    public class AccountEntity
+    {
+        [Key]
+        [Column("id")]
+        public long Id { get; set; }
+
+        [Column("full_name")]
+        public string FullName { get; set; }
+
+        [Column("address")]
+        public string Address { get; set; }
+
+        [Column("role")]
+        public string Role { get; set; }
+
+        public List<RequestAssetEntity> RequestAssets { get; set; }
+
+        public List<AssetHistoryEntity> AssetHistories { get; set; }
+    }
+}
---
+++ Asset-Management.ViewModel/RequestListResponse.cs
@@ -0,0 +1,12 @@
+﻿namespace Asset_Management.ViewModel
+{
+    public class RequestListResponse
+    {
+        public long Id { get; set; }
+        public long PicId { get; set; }
+        public string FullName { get; set; }
+        public string Specification { get; set; }
+        public DateTime RequestDate { get; set; }
+        public string Status { get; set; }
+    }
+}
